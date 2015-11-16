cc69d4a67c1c94e7c37350c4e38f96e38fbce85c
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-16 13:28:57 -0500

    use token, adds data-store

--- cli.js
+++ cli.js
@@ -2,68 +2,100 @@
 
 process.title = 'enable-travis';
 
+var argv = require('minimist')(process.argv.slice(2));
 var chalk = require('chalk');
 var symbol = require('log-symbols');
 var inquirer = require('inquirer');
+var DataStore = require('data-store');
+var store = new DataStore('enable-travis');
+
+if (argv.set) {
+  var args = argv.set.split(/[=:,]/);
+  console.log(args)
+  store.set.apply(store, args);
+  process.exit(1);
+}
+
+if (argv.get) {
+  console.log(store.get(argv.get));
+  process.exit(1);
+}
 
 var repo = require('git-repo-name');
 var user = require('git-user-name');
 var name = require('git-username');
 var pkg = require('load-pkg');
 
-var username = name() || user();
+var username = name() || user() || store.get('username');
+// var password = store.get('password');
+var token = store.get('GITHUB_OAUTH_TOKEN');
 var repository = repo() || pkg && pkg.name;
 var enable = require('./');
 
 
 run(function (env) {
-  enable(env.repo, env, function (err, res) {
+  enable(env, function (err, res) {
     if (err) {
+      console.log(err);
       console.log(chalk.yellow('Oops! Travis doesn\'t seem know about this project yet.'));
-      console.log(chalk.yellow('Check to make sure your projects are up-to-date.'));
+      console.log(chalk.yellow('Use "https://github.com/jonschlinkert/sync-travis" to '));
+      console.log(chalk.yellow('update Travis CI with your latest GitHub projects.'));
       process.exit(0);
     }
     if (res && res.result) {
-      symbol.success + ' ' + console.log(chalk.green(env.repo, 'has been enabled!'));
+      console.log(symbol.success + ' ' + chalk.green(env.repo), 'has been enabled!');
     }
   });
 });
 
 function run(cb) {
-  console.log(chalk.cyan('Please provide your github username and password:'));
-  console.log(chalk.gray('(answers are never stored):'));
+  console.log('Please provide the repo to enable, your github username and auth token:');
+  // console.log(chalk.gray('(answers are never stored):'));
   console.log();
 
   var prompts = [];
 
   prompts.push({
-    type: "input",
-    name: 'username',
-    message: chalk.bold('username'),
-    default: username
-  });
-
-  prompts.push({
-    type: "password",
-    name: 'password',
-    message: chalk.bold('password'),
-    default: null
-  });
-
-  prompts.push({
-    type: "input",
+    type: 'input',
     name: 'repo',
     message: chalk.bold('owner/repo'),
     default: username + '/' + repository
   });
 
-  inquirer.prompt(prompts, function (answer) {
-    if(answer) {
-      if (answer.repo && answer.repo.indexOf('/') === -1) {
+  prompts.push({
+    type: 'input',
+    name: 'username',
+    message: chalk.bold('username'),
+    default: username
+  });
+
+  // prompts.push({
+  //   type: 'password',
+  //   name: 'password',
+  //   message: chalk.bold('password'),
+  //   default: password
+  // });
+
+  prompts.push({
+    type: 'password',
+    name: 'GITHUB_OAUTH_TOKEN',
+    message: chalk.bold('GitHub auth token'),
+    default: token
+  });
+
+  inquirer.prompt(prompts, function (answers) {
+    for (var key in answers) {
+      if (key !== repo) {
+        store.set(key, answers[key]);
+      }
+    }
+
+    if(answers) {
+      if (answers.repo && answers.repo.indexOf('/') === -1) {
         console.log(chalk.red('github repo must be in the form of `owner/repo'));
         process.exit(1);
       }
-      cb(answer);
+      cb(answers);
     } else {
       console.log(chalk.green('\n  Got it. All is good.'));
       process.exit(0);
--- index.js
+++ index.js
@@ -19,26 +19,32 @@ module.exports = enable;
  * Enable a travis project
  */
 
-function enable(repo, env, cb) {
+function enable(env, cb) {
   var travis = new Travis({version: '2.0.0'});
   env = env || {};
 
-  travis.authenticate({
-    username: env.username,
-    password: env.password
+  travis.auth.github.post({
+    github_token: env.GITHUB_OAUTH_TOKEN
   }, function (err, res) {
     if (err) return cb(err);
-    var segs = repo.split('/');
 
-    travis.repos(segs[0], segs[1]).get(function (err, result) {
+    travis.authenticate({
+      access_token: res.access_token
+    }, function (err) {
+
       if (err) return cb(err);
+      var segs = env.repo.split('/');
 
-      travis.hooks(result.repo && result.repo.id).put({
-        hook: {active: true}
-
-      }, function (err, content) {
+      travis.repos(segs[0], segs[1]).get(function (err, res) {
         if (err) return cb(err);
-        cb(null, content);
+
+        travis.hooks(res.repo && res.repo.id).put({
+          hook: {active: true}
+
+        }, function (err, content) {
+          if (err) return cb(err);
+          cb(null, content);
+        });
       });
     });
   });
