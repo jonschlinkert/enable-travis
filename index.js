111800e081edd667ff0b83c24db4d387c9ba34ab
doowb <brian.woodward@gmail.com>
2017-07-23 22:49:47 -0400

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * enable-travis <https://github.com/jonschlinkert/enable-travis>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
